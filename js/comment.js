118cf6880f9f56f46fa990d639c68295e07dc930
Maks <2102maksim2004@gmail.com>
2023-11-20 00:44:22 +0500

    module8-task1

---
+++ js/comment.js
@@ -0,0 +1,20 @@
+const createComment = (comment) => {
+  const newComment = document.createElement('li');
+  newComment.classList.add('.social__comments');
+  const photoUser = document.createElement('img');
+  photoUser.classList.add('social__picture');
+  photoUser.src = comment.avatar;
+  photoUser.alt = comment.name;
+  photoUser.width = 35;
+  photoUser.hight= 35;
+  newComment.appendChild(photoUser);
+
+  const textPhoto = document.createElement('p');
+  textPhoto.classList.add('social__text');
+  textPhoto.textContent = comment.message;
+  newComment.appendChild(textPhoto);
+
+  return newComment;
+};
+
+export{createComment};
--- js/render-photo.js
+++ js/render-photo.js
@@ -1,4 +1,5 @@
 import { GeneratedArrayUsers } from './create-users.js';
+import { createBigPicture } from './full-screen-image.js';
 
 const  pictureListElement = document.querySelector('.pictures');
 
@@ -6,14 +7,29 @@ const pictureTemplate = document.querySelector('#picture').content.querySelector
 
 const createArrayPicture = GeneratedArrayUsers();
 
-const pictureFragment = document.createDocumentFragment();
+const picturesItems = [];
 
-createArrayPicture.forEach((picture) => {
+const createPictureUser = (picture) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = picture.url;
   pictureElement.querySelector('.picture__img').alt = picture.description;
   pictureElement.querySelector('.picture__likes').textContent = picture.likes;
   pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
-  pictureFragment.appendChild(pictureElement);
-});
-pictureListElement.appendChild(pictureFragment);
+
+  createBigPicture(pictureElement, picture);
+
+  return pictureElement;
+};
+
+//отрисовка фотографии на сайте
+const showPictures = (pictures) => {
+  const pictureFragment = new DocumentFragment;
+  pictures.forEach((picture) => pictureFragment.appendChild(picture));
+  pictureListElement.appendChild(pictureFragment);
+};
+
+createArrayPicture.forEach((picture) => picturesItems.push(createPictureUser(picture)));
+
+showPictures(picturesItems);
+
+
