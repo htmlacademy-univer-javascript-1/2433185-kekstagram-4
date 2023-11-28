2ff04038973d0db3a7b7112317a7f59217913fb1
Maks <2102maksim2004@gmail.com>
2023-11-28 19:10:57 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
+
 import './full-screen-image.js';
 import './render-photo.js';
-=======
-import { GeneratedArrayUsers } from './create-users';
-import { picturesList } from './render-photo';
-const photos = GeneratedArrayUsers();
-picturesList(photos);
->>>>>>> af80f304f49fd6e27f8997fd52dd93ae9cfe98f2
+
--- js/render-photo.js
+++ js/render-photo.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { GeneratedArrayUsers } from './create-users.js';
 import { showBigPicture } from './full-screen-image.js';
 
@@ -33,27 +33,3 @@ createArrayPicture.forEach((picture) => picturesItems.push(createPictureUser(pic
 
 showPictures(picturesItems);
 
-
-=======
-const  pictureListElement = document.querySelector('.pictures');
-
-const pictureTemplate = document.querySelector('#picture').content;
-
-let pictures = null;
-
-export const picturesList = (photo) => {
-  pictures = photo.slice();
-  if(pictures){
-    const pictureFragment = document.createDocumentFragment();
-    pictures.forEach((picture) => {
-      const pictureElement = pictureTemplate.cloneNode(true);
-      pictureElement.querySelector('.picture__img').src = picture.url;
-      pictureElement.querySelector('.picture__img').alt = picture.description;
-      pictureElement.querySelector('.picture__likes').textContent = picture.likes;
-      pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
-      pictureFragment.appendChild(pictureElement);
-    });
-    pictureListElement.appendChild(pictureFragment);
-  }
-};
->>>>>>> af80f304f49fd6e27f8997fd52dd93ae9cfe98f2
